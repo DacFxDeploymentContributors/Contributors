cf8795fe08c0fbd0ffb815ee2049d3d673d6c6a8
Ed Elliott <goeddieuk@gmail.com>
2015-11-06 07:17:55 +0000

    initial commit

---
+++ src/StopDeploymentsOnBreakingProcedureChanges/Tests/IntegrationTests/Framework/DacpacDeploy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace IntegrationTests.Framework
+{
+    internal static class DacpacDeploy
+    {
+        public static string Deploy(string dacpac, string server, string database, string outputPath = ".\\script.sql", string additionalDpeloymentArgs = "")
+        {
+            const string destinationFile = "sqlpackage\\StopDeploymentsOnBreakingProcedureChanges.dll";
+
+            if (File.Exists(destinationFile))
+            {
+                File.Delete(destinationFile);
+            }
+
+            if (File.Exists(outputPath))
+            {
+                File.Delete(outputPath);
+            }
+
+            File.Copy(".\\StopDeploymentsOnBreakingProcedureChanges.dll", destinationFile);
+
+            var args =
+                string.Format(
+                    "/action:script /sf:{0} /tsn:{1} /tdn:{2} /op:{3} /p:AllowIncompatiblePlatform=true /p:AdditionalDeploymentContributors=StopDeploymentsOnBreakingProcedureChanges /p:AdditionalDeploymentContributorArguments=zEasyDebugStopDeploymentsOnBreakingProcedureChanges=true;{4}",
+                    dacpac, server, database, outputPath, additionalDpeloymentArgs);
+
+
+            var startupInfo = new ProcessStartInfo();
+            startupInfo.FileName = "sqlpackage\\sqlpackage.exe";
+            startupInfo.Arguments = args;
+            startupInfo.RedirectStandardError = true;
+            startupInfo.RedirectStandardOutput = true;
+            startupInfo.UseShellExecute = false;
+
+            var p = Process.Start(startupInfo);
+            p.WaitForExit();
+
+            var s = "StdError: \r\n" + p.StandardError.ReadToEnd();
+            s += "\r\nStdOut: \r\n" + p.StandardOutput.ReadToEnd();
+
+           
+            Console.WriteLine("Messages: " + s);
+
+            return s;
+        }
+    }
+}
---
+++ src/StopDeploymentsOnBreakingProcedureChanges/Tests/IntegrationTests/Stop_Deploy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using IntegrationTests.Framework;
+using NUnit.Framework;
+
+namespace IntegrationTests
+{
+    [TestFixture]
+    public class Stop_Deploy
+    {
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            Database.CleanOrCreateDatabase();
+        }
+
+        [Test]
+        public void When_Parameter_Deleted()
+        {
+            var messages = DacpacDeploy.Deploy(@"..\..\..\TestDacpacDeploy\bin\Debug\TestDacpacDeploy.dacpac", Database.server_name, Database.db_name);
+
+            Assert.IsTrue(messages.Contains(@"StopDeploymentsOnBreakingProcedureChanges: The procedure [dbo].[TestoRemoveParameter] has had a parameter removed, parameter name: [dbo].[TestoRemoveParameter].[@jj]"));
+
+        }
+
+        [Test]
+        public void When_Parameter_Add_With_No_Default()
+        {
+            var messages = DacpacDeploy.Deploy(@"..\..\..\TestDacpacDeploy\bin\Debug\TestDacpacDeploy.dacpac", Database.server_name, Database.db_name);
+            Assert.IsTrue(messages.Contains(@"StopDeploymentsOnBreakingProcedureChanges: The procedure [dbo].[TestoAddParameterNoDefault] has had an additional parameter but no default, parameter name: [dbo].[TestoAddParameterNoDefault].[@b]"));
+        }
+
+        [Test]
+        public void When_Parameter_Default_Remoced()
+        {
+            var messages = DacpacDeploy.Deploy(@"..\..\..\TestDacpacDeploy\bin\Debug\TestDacpacDeploy.dacpac", Database.server_name, Database.db_name);
+            Assert.IsTrue(messages.Contains(@"StopDeploymentsOnBreakingProcedureChanges: The procedure [dbo].[TestoRemoveParameter] has had a parameter removed, parameter name: [dbo].[TestoRemoveParameter].[@jj]"));
+
+        }
+    }
+}
